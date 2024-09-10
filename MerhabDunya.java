51594a29c1cec4e144ce1c4d064ecbcd14f6fba5
Ömer Faruk Uzkal <147771249+ofuzkal@users.noreply.github.com>
2024-09-10 12:30:24 +0300

    Add files via upload

---
+++ MerhabDunya.java
@@ -0,0 +1,18 @@
+
+public class MerhabDunya {
+
+	public static void main(String[] args) {
+
+		System.out.println("Merhaba Dünya");
+		System.out.println("Hoş geldiniz");
+		
+		int x = 5;
+		
+		System.out.println("x Değeri: " + x);
+		
+		float y = 8.6F;
+		System.out.println("y Değeri: " + y);
+
+	}
+
+}
